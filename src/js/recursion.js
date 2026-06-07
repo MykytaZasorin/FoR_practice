b079614b6dc5d39c2c760622ae0947e9188bdc2e
MykytaZasorin <mykytazasorin@gmail.com>
2026-06-07 23:46:11 +0300

    recurs20

--- src/js/recursion.js
+++ src/js/recursion.js
@@ -76,12 +76,62 @@ function countChar(str, char) {
   }
 }
 // Task-11
+function findMax(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  return Math.max(arr[0], findMax(arr.slice(1)));
+}
 // Task-12
+function isPalindrome(str) {
+  if (str.length <= 1) {
+    return true;
+  }
+  if (str[0] !== str[str.length - 1]) {
+    return false;
+  }
+  return isPalindrome(str.slice(1, -1));
+}
 // Task-13
+function multiplyArray(arr) {
+  if (arr.length === 0) return 1;
+  return arr[0] * multiplyArray(arr.slice(1));
+}
 // Task-14
+function containsNumber(arr, num) {
+  if (arr.length === 0) return false;
+  if (arr[0] === num) return true;
+  return containsNumber(arr.slice(1), num);
+}
 // Task-15
+function printArrayReverse(arr) {
+  if (arr.length === 0) return;
+  printArrayReverse(arr.slice(1));
+  console.log(arr[0]);
+}
 // Task-16
+function fib(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
 // Task-17
+function joinWords(arr) {
+  if (arr.length === 1) return arr[0];
+  return arr[0] + " " + joinWords(arr.slice(1));
+}
 // Task-18
+function countDigits(num) {
+  if (num < 10) return 1;
+  return 1 + countDigits(Math.floor(num / 10));
+}
 // Task-19
+function sumDigits(num) {
+  if (num < 10) return num;
+  return (num % 10) + sumDigits(Math.floor(num / 10));
+}
 // Task-20
+function repeatStr(str, n) {
+  if (n === 1) return str;
+  return str + repeatStr(str, n - 1);
+}
