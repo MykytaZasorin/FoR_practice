e5491d86bf55ad2d5835b947e1ca027bd867481b
MykytaZasorin <mykytazasorin@gmail.com>
2026-06-09 23:30:50 +0300

    upd

--- src/js/promises.js
+++ src/js/promises.js
@@ -61,6 +61,85 @@ async function getData() {
   }
 }
 
-// Promise.rase - не знаю що це таке
+async function getFastestData() {
+  try {
+    const fastestResponse = await Promise.race([
+      fetchUsers(),
+      fetchPosts(),
+      fetchComments(),
+    ]);
+    return fastestResponse;
+  } catch (er) {
+    console.log(er);
+  }
+}
 
-// Promise.allSettled - не знаю також
+async function getAllDataSafety() {
+  try {
+    const results = await Promise.allSettled([
+      fetchUsers(),
+      fetchPosts(),
+      fetchComments(),
+    ]);
+    return fastestResponse;
+  } catch (er) {
+    console.log(er);
+  }
+}
+
+const ids = [1, 2, 3, 4];
+const ids = [1, 2, 3, 4];
+
+async function processSequentially() {
+  for (const id of ids) {
+    console.log(`Почали відправку для ID: ${id}`);
+    const result = await sendData(id);
+    console.log(`Успішно завершено для ID: ${id}`);
+  }
+}
+
+async function sendData(id) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      method: "PUT",
+      body: JSON.stringify({ title: "newData" }),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    },
+  );
+  return response.json();
+}
+
+processSequentially();
+
+function readFilePromise(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+readFilePromise("file.txt")
+  .then((data) => console.log("Проміс повернув дані:", data))
+  .catch((err) => console.error("Сталася помилка:", err));
+
+function fetchWithTimeout(url, timeout) {
+  const timeoutPromise = new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error("Timeout Error"));
+    }, timeout);
+  });
+
+  const fetchPromise = fetch(url).then((response) => response.json());
+
+  return Promise.race([fetchPromise, timeoutPromise]);
+}
+
+fetchWithTimeout("https://jsonplaceholder.typicode.com/photos", 2000)
+  .then((data) => console.log("Дані отримано успішно:", data))
+  .catch((err) => console.error("Помилка:", err.message));
